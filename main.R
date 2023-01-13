d36f891f56c31b9a7794ea041f40d708e65fb8bb
Anne Staff <anne.staff@pm.me>
2023-01-13 13:56:56 +0100

    yet more worrrrk

--- main.R
+++ main.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+set.seed(42)
 # initial values
 K <- 950 # equilibrium density
 lambda <- 1.5 # growth rate
@@ -22,30 +24,42 @@ calc_fitness <- function(N, K, lambda, c) {
 # test
 # with(population[1,], calc_fitness(N_init, K, lambda, c))
 
+vector_fitness <- function(input, N) {
+  lambda = input[2]
+  if (any(lambda == 1)) {
+    lambda <- 1.0000000001
+  }
+  res <- lambda / (1 + (lambda - 1)*((N/input[1])**((1-input[3])*lambda/(lambda-1))))
+  return(res)
+}
+
 # run
 fitness <- with(population[1,], calc_fitness(N_init, K, lambda, c))
 population$fitness <- rep(fitness, N_init)
 
 # offspring per individual without mutation
-offspring <- function(individual, N) {
-  n_offspring <- rpois(1, individual$fitness)
+offspring <- function(individual) {
+  n_offspring <- rpois(1, individual[4])
   if (is.na(n_offspring)) {
     return(vector())
   } else {
-    offspring <- data.frame(K=rep(individual$K, n_offspring), lambda=rep(individual$lambda, n_offspring), c=rep(individual$c, n_offspring), fitness=rep(individual$fitness, n_offspring))
+    offspring <- data.frame(K=rep(individual[1], n_offspring), lambda=rep(individual[2], n_offspring), c=rep(individual[3], n_offspring), fitness=rep(individual[4], n_offspring))
     return(offspring)
   }
 }
 # test
-# offspring(population[1,], N_init)
+# offspring(population[1,])
 
 # next generation
 next_generation <- function(population){
-  next_gen <- data.frame(K=vector(), lambda=vector(), c=vector(), fitness=vector())
-  for (i in 1:nrow(population)){
-    o <- offspring(population[i,], nrow(population))
-    next_gen <- rbind(next_gen, o)
-  }
+  # next_gen <- data.frame(K=vector(), lambda=vector(), c=vector(), fitness=vector())
+  #for (i in 1:nrow(population)){
+  #  o <- offspring(population[i,], nrow(population))
+  #  next_gen <- rbind(next_gen, o)
+  #}
+  next_gen <- apply(population, 1, offspring)
+  next_gen <- bind_rows(next_gen)
+  next_gen$fitness <- apply(next_gen, 1, vectorised_fitness, N=nrow(next_gen))
   return(next_gen)
 }
 # test
@@ -70,11 +84,7 @@ mutate <- function(individual){
 }
 
 next_generation_with_mutation <- function(population){
-  next_gen <- data.frame(K=vector(), lambda=vector(), c=vector(), fitness=vector())
-  for (i in 1:nrow(population)){
-    o <- offspring(population[i,], nrow(population))
-    next_gen <- rbind(next_gen, o)
-  }
+  next_gen <- next_generation(population)
   n_mutations <- rpois(1, nrow(next_gen)*mu)
   mutants <- sample(nrow(next_gen), n_mutations, replace=FALSE)
   if(length(mutants) > 0) {
@@ -113,15 +123,49 @@ run_with_mutation <- function(x, population) {
   return(generations_with_mutation)
 }
 
-x <- 20
+x <- 300
 generations <- run(x, population)
 generations_with_mutation <- run_with_mutation(x, population)
+
+run_infinitely_while_saving_to_file <- function(population) {
+  cnt <- 0
+  population_with_mutation <- population
+  # with mutation
+  generations_with_mutation <- data.frame(
+    N=nrow(population_with_mutation), 
+    K_means=mean(population_with_mutation$K), 
+    K_sd=sd(population_with_mutation$K), 
+    lambda_means=mean(population_with_mutation$lambda), 
+    lambda_sd=sd(population_with_mutation$lambda), 
+    c_means=mean(population_with_mutation$c), 
+    c_sd=sd(population_with_mutation$c), 
+    fitness_means=mean(population_with_mutation$fitness), 
+    fitness_sd=sd(population_with_mutation$fitness), 
+    r=nrow(population_with_mutation)/mean(population_with_mutation$K))
+  while (TRUE) {
+    population_with_mutation <- next_generation_with_mutation(population_with_mutation)
+    generations_with_mutation <- rbind(generations_with_mutation, 
+                                       c(nrow(population_with_mutation), 
+                                         mean(population_with_mutation$K), 
+                                         sd(population_with_mutation$K), 
+                                         mean(population_with_mutation$lambda), 
+                                         sd(population_with_mutation$lambda), 
+                                         mean(population_with_mutation$c), 
+                                         sd(population_with_mutation$c), 
+                                         mean(population_with_mutation$fitness), 
+                                         sd(population_with_mutation$fitness),
+                                         nrow(population_with_mutation)/mean(population_with_mutation$K)))
+    write.table(generations_with_mutation, file=test.txt)
+    cnt <- cnt + 1
+    print(cnt)
+  }
+}
 
 plot(1:x, generations$N, type="b", pch=19, col="blue", xlim=c(1, x), ylim=c(0, max(c(generations$N, generations_with_mutation$N))), xlab="Number of generations", ylab="Population size", main="Evolution of population sizes over generations with and without mutations")
 par(new = T)
 plot(1:x, generations_with_mutation$N, type="b", pch=18, col="red", xlim=c(1, x), ylim=c(0, max(c(generations$N, generations_with_mutation$N))), xlab="Number of generations", ylab="Population size")
 
-legend(1, max(c(generations$N, generations_with_mutation$N))-200, legend=c("Without mutation", "With mutation"), col=c("blue", "red"), lty=1:2, cex=0.8)
+legend(1, 200, legend=c("Without mutation", "With mutation"), col=c("blue", "red"), lty=1:2, cex=0.8)
 
 # plot relative density of population with mutation
 plot(1:x, generations_with_mutation$r, type="b", pch=20, col="blue", xlab="Time in generations", ylab="Relative density (N/mean(K)", main="Evolution of the relative density over generations")
